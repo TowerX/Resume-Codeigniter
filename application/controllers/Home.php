f033b1514beb1121fa487613f1cdd65afd9be2e7
TowerX <jptb86@gmail.com>
2018-07-20 00:14:11 -0500

    Initial Upload

---
+++ application/controllers/Home.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{	
+
+		// Meta tags
+		$data['title'] = 'Title of the page'; // This is the title of the page e.g 'Resume - Start Bootstrap Theme'.
+		$data['description'] = 'Description of the site'; // This is the <head> meta description field.
+		$data['author'] = 'Author of the site'; // This is the <head> meta author field.
+
+		// About Section
+		$data['first_name'] = 'Jay';
+		$data['last_name'] = 'Torres';
+		$data['address'] = 'Saint Paul, MN 55119';
+		$data['your_email'] = 'name@email.com';
+		$data['about_info'] = 'During my time as a freelance web developer I have learned the importance of working with a team while collaborating with various artists and other programmers on multiple projects. As a freelance developer, I have fine-tuned my craft to deliver results that my clients have needed.';
+
+
+
+		$this->load->view('header', $data); // <head> block of the HTML.
+		$this->load->view('navigation', $data); // <nav> block of the HTML.
+		$this->load->view('home', $data); // The actual content of the page.
+		$this->load->view('footer', $data); // The footer of the document including closing </body> and any links at the bottom of the document.
+	}
+}
---
+++ application/views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="<?php echo $description; ?>">
+    <meta name="author" content="<?php echo $author; ?>">
+
+    <title><?php echo $title; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+    <link href="<?php base_url('vendor/devicons/css/devicons.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('vendor/simple-line-icons/css/simple-line-icons.css'); ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('css/resume.min.css'); ?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
