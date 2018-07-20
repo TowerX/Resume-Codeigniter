a248924ed2ab3a1969d2527acdec12da1c4a3e4d
TowerX <jptb86@gmail.com>
2018-07-20 00:39:50 -0500

    Config Update
    Comment as a reminder

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+// CHANGE THIS URL TO POINT AT YOURS - It wont work if left untouched.
 $config['base_url'] = 'http://localhost/Resume-Codeigniter/';
 
 /*
