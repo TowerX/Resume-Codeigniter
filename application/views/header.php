ee380a94ae0ab8cd92c981ae9c08e26ad746b2a3
TowerX <jptb86@gmail.com>
2018-07-20 00:27:43 -0500

    Fixes

--- application/views/header.php
+++ application/views/header.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
     <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
-    <link href="<?php base_url('vendor/devicons/css/devicons.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('vendor/devicons/css/devicons.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('vendor/simple-line-icons/css/simple-line-icons.css'); ?>" rel="stylesheet">
 
     <!-- Custom styles for this template -->
