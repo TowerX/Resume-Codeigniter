8c93dc6b27cec939b859e5f120c98966a0fb11b9
TowerX <jptb86@gmail.com>
2020-10-01 00:24:21 -0400

    Fix: Broken links

--- application/views/header.php
+++ application/views/header.php
@@ -11,17 +11,17 @@
     <title><?php echo $title; ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('vendor/devicons/css/devicons.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('vendor/simple-line-icons/css/simple-line-icons.css'); ?>" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@master/devicon.min.css">
+    <!--<link href="<?php echo base_url('vendor/simple-line-icons/css/simple-line-icons.css'); ?>" rel="stylesheet">-->
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url('css/resume.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('css/resume.css'); ?>" rel="stylesheet">
 
   </head>
 
